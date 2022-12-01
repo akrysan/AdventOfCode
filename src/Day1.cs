0252ca2b9ae02602c26659806382edf21bdda72b
Anatoliy Krysan <akrysan@gmail.com>
2022-12-01 21:42:56 +0200

    day 1

---
+++ src/Day1.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+
+namespace AdventOfCode
+{
+    public class Day1
+    {
+        public static int Solve()
+        {
+            var input = System.IO.File.ReadAllText(@"input\day1.txt");
+
+            var maxCalories = input
+                .Split("\r\n\r\n")
+                .Select(x => x.Split("\r\n"))
+                .Select(y => y.Select(z => int.Parse(z)).Sum())
+                .OrderByDescending(x => x)
+                .Take(3)
+                .Sum();
+
+
+            return maxCalories;
+        }
+    }
+}
